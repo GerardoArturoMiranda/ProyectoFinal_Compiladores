0c22993f1eeb3de2bf1e4584bb5ab7537eedbbbd
Akio Olvera <akiooarao@gmail.com>
2021-05-10 13:15:19 -0600

    A few notes

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Installing
 
 ### Previous
-Add AdventureWorks.bak inside `src/Docker`  
-I wasn't able to upload it to the repo because it's too big.
+Add AdventureWorks.bak inside `src/Docker`.  
+I wasn't able to upload it to the repo because it's too big.  
+Replace restore and restore1 with specific OS (Windows/Mac).
 
 ### Build docker container
 May take several minutes
--- src/Docker/Conexion/consulta2.sql
+++ src/Docker/Conexion/consulta2.sql
@@ -1,7 +1,7 @@
 WITH DirectReports (ManagerID, EmployeeID, Title, DeptID, Level)
 AS
 (
--- Anchor member definition
+-- Anchor member definition - CASO BASE
     SELECT e.ManagerID, e.EmployeeID, e.Title, edh.DepartmentID, 
         0 AS Level
     FROM HumanResources.Employee AS e
@@ -9,7 +9,7 @@ AS
         ON e.EmployeeID = edh.EmployeeID AND edh.EndDate IS NULL
     WHERE ManagerID IS NULL
     UNION ALL
--- Recursive member definition
+-- Recursive member definition - RECURSIVIDAD
     SELECT e.ManagerID, e.EmployeeID, e.Title, edh.DepartmentID,
         Level + 1
     FROM HumanResources.Employee AS e
@@ -19,6 +19,7 @@ AS
         ON e.ManagerID = d.EmployeeID
 )
 -- Statement that executes the CTE
+-- INSERT INTO DirectReports(?) para persistir datos
 SELECT ManagerID as ManagerID, EmployeeID as EmployeeID, Title as Title, Level as Level
 FROM DirectReports
 INNER JOIN HumanResources.Department AS dp
